fc4bb937881466a88e408f660ce4d8ad18c11bad
Joe Ferguson <joe@infosiftr.com>
2014-11-20 16:56:08 -0800

    Add a micro buildpack with pkgs for java and golang to share

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -15,10 +15,22 @@ url='git://github.com/docker-library/docker-buildpack-deps'
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
 for version in "${versions[@]}"; do
-	commit="$(git log -1 --format='format:%H' "$version")"
 	versionAliases=( $version ${aliases[$version]} )
 	
 	echo
+	for variant in micro; do
+		commit="$(git log -1 --format='format:%H' "$version/$variant")"
+		for va in "${versionAliases[@]}"; do
+			if [ "$va" = 'latest' ]; then
+				va="$variant"
+			else
+				va="$va-$variant"
+			fi
+			echo "$va: ${url}@${commit} $version/$variant"
+		done
+	done
+	
+	commit="$(git log -1 --format='format:%H' "$version")"
 	for va in "${versionAliases[@]}"; do
 		echo "$va: ${url}@${commit} $version"
 	done
