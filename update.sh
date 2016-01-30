9edcd31dd2e3611fbc61dcc6f7da6b1454b1948d
Alex Bell <callbacknull@gmail.com>
2016-01-30 12:16:18 -0500

    feat: Adding multi distro support
    changes:
    * Dockerfile templates are moved to `templates/`
    * Templates follow the naming pattern of `Dockerfile-$distro.template`
    * The Ubuntu template is a symbolic link to the debian template
    * Adds a template for the `alpine` distro
    * Updates `update.sh` for alpine and the new template naming conventions

--- update.sh
+++ update.sh
@@ -11,25 +11,30 @@ versions=( "${versions[@]%/}" )
 
 debian="$(curl -fsSL 'https://github.com/docker-library/official-images/blob/master/library/debian')"
 ubuntu="$(curl -fsSL 'https://github.com/docker-library/official-images/blob/master/library/ubuntu')"
+alpine="$(curl -fsSL 'https://github.com/docker-library/official-images/blob/master/library/alpine')"
 
 travisEnv=
 for version in "${versions[@]}"; do
-	if echo "$debian" | grep -q "$version:"; then
+    if [ "$version" == "templates" ]; then
+	    continue;
+	elif echo "$debian" | grep -q "$version:"; then
 		dist='debian'
 	elif echo "$ubuntu" | grep -q "$version:"; then
 		dist='ubuntu'
+	elif echo "$alpine" | grep -q "$version:"; then
+	    dist='alpine'
 	else
 		echo >&2 "error: cannot determine repo for '$version'"
 		exit 1
 	fi
 	for variant in curl scm ''; do
-		src="Dockerfile.template${variant:+-$variant}"
+		src="Dockerfile-$dist.template${variant:+-$variant}"
 		trg="$version${variant:+/$variant}/Dockerfile"
 		mkdir -p "$(dirname "$trg")"
 		( set -x && sed '
 			s!DIST!'"$dist"'!g;
 			s!SUITE!'"$version"'!g;
-		' "$src" > "$trg" )
+		' "templates/$src" > "$trg" )
 	done
 	travisEnv+='\n  - VERSION='"$version"
 done
