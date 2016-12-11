6ed521fc68de3d34274bf075ee9c516547c8f5ff
You-Sheng Yang <vicamo@gmail.com>
2016-12-11 12:58:52 +0000

    fix update.sh to generate multiarch .travis.yml

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( "$@" )
 if [ ${#versions[@]} -eq 0 ]; then
-	versions=( */ )
+	versions=( */*/ )
 fi
 versions=( "${versions[@]%/}" )
 
@@ -14,21 +14,28 @@ ubuntu="$(curl -fsSL 'https://raw.githubusercontent.com/docker-library/official-
 
 travisEnv=
 for version in "${versions[@]}"; do
-	if echo "$debian" | grep -qE "\b$version\b"; then
+	if echo "$debian" | grep -qE "\b${version%/*}\b"; then
 		dist='debian'
-	elif echo "$ubuntu" | grep -qE "\b$version\b"; then
+	elif echo "$ubuntu" | grep -qE "\b${version%/*}\b"; then
 		dist='ubuntu'
 	else
 		echo >&2 "error: cannot determine repo for '$version'"
 		exit 1
 	fi
+
+	if ! grep -q '^# GENERATED' $version/Dockerfile; then
+		travisEnv+='\n  - VERSION='"$version"
+		continue
+	fi
+
 	for variant in curl scm ''; do
 		src="Dockerfile${variant:+-$variant}.template"
 		trg="$version${variant:+/$variant}/Dockerfile"
 		mkdir -p "$(dirname "$trg")"
 		( set -x && sed '
 			s!DIST!'"$dist"'!g;
-			s!SUITE!'"$version"'!g;
+			s!SUITE!'"${version%/*}"'!g;
+			s!ARCH!'"${version#*/}"'!g;
 		' "$src" > "$trg" )
 	done
 	travisEnv+='\n  - VERSION='"$version"
