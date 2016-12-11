1482e4b4f1e676fc45b3f27c5bf028670dfa0f92
You-Sheng Yang <vicamo@gmail.com>
2016-12-11 12:48:37 +0000

    bring back update.sh from upstream

--- update.sh
+++ update.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+set -eo pipefail
 
-echo >&2 "see Makefile instead."
-exit 1
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+debian="$(curl -fsSL 'https://raw.githubusercontent.com/docker-library/official-images/master/library/debian')"
+ubuntu="$(curl -fsSL 'https://raw.githubusercontent.com/docker-library/official-images/master/library/ubuntu')"
+
+travisEnv=
+for version in "${versions[@]}"; do
+	if echo "$debian" | grep -qE "\b$version\b"; then
+		dist='debian'
+	elif echo "$ubuntu" | grep -qE "\b$version\b"; then
+		dist='ubuntu'
+	else
+		echo >&2 "error: cannot determine repo for '$version'"
+		exit 1
+	fi
+	for variant in curl scm ''; do
+		src="Dockerfile${variant:+-$variant}.template"
+		trg="$version${variant:+/$variant}/Dockerfile"
+		mkdir -p "$(dirname "$trg")"
+		( set -x && sed '
+			s!DIST!'"$dist"'!g;
+			s!SUITE!'"$version"'!g;
+		' "$src" > "$trg" )
+	done
+	travisEnv+='\n  - VERSION='"$version"
+done
+
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
