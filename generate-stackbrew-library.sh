4f766db54f5179aca1a841e4036d6d6746ac79d9
Tianon Gravi <admwiggin@gmail.com>
2021-07-27 11:30:03 -0700

    Exclude upper variants on focal+riscv64 (missing mercurial and likely others)

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -90,10 +90,16 @@ for version; do
 		variantAliases=( "${versionAliases[@]/%/${variant:+-$variant}}" )
 		variantAliases=( "${variantAliases[@]//latest-/}" )
 
+		variantArches="$arches"
+		if [ "$version" = 'ubuntu/focal' ] && [ "$variant" != 'curl' ]; then
+			# focal on riscv64 doesn't have mercurial, so we just exclude the upper focal variants from riscv64 entirely
+			variantArches="$(sed -r -e 's/ riscv64 / /g' <<<" $variantArches ")"
+		fi
+
 		echo
 		cat <<-EOE
 			Tags: $(join ', ' "${variantAliases[@]}")
-			Architectures: $(join ', ' $arches)
+			Architectures: $(join ', ' $variantArches)
 			GitCommit: $commit
 			Directory: $version${variant:+/$variant}
 		EOE
